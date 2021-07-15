7b47bd7ea3e5f7e0c4c925ee17f10a162409e2c0
Victor Perez <victorperez099@yahoo.com>
2021-07-14 23:58:52 -0600

    Finished GameDao and need to query for UserDao.

---
+++ app/src/main/java/edu/cnm/deepdive/tilematch/model/dao/UserDao.java
@@ -0,0 +1,43 @@
+package edu.cnm.deepdive.tilematch.model.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Update;
+import edu.cnm.deepdive.tilematch.model.entity.Game;
+import edu.cnm.deepdive.tilematch.model.entity.User;
+import io.reactivex.Single;
+import java.util.Collection;
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    Single<Long> insert(User user);
+
+    @Insert
+    Single<List<Long>> insert(User... user);
+
+    @Insert
+    Single<List<Long>> insert(Collection<? extends User> user);
+
+    @Update
+    Single<Integer> update(User user);
+
+    @Update
+    Single<Integer> update(User... user);
+
+    @Update
+    Single<Integer> update(Collection<? extends User> user);
+
+    @Delete
+    Single<Integer> delete(User user);
+
+    @Delete
+    Single<Integer> delete(User... user);
+
+    @Delete
+    Single<Integer> delete(Collection<? extends User> user);
+
+}
