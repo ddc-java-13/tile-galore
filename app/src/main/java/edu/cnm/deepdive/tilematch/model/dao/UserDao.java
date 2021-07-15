d96def029019e99b6d47bbf8356ce24881dee61f
Victor Perez <victorperez099@yahoo.com>
2021-07-15 14:59:18 -0600

    Put query for UserDao. Put TypeConverters but need one for my enum and perhaps a query as well.

--- app/src/main/java/edu/cnm/deepdive/tilematch/model/dao/UserDao.java
+++ app/src/main/java/edu/cnm/deepdive/tilematch/model/dao/UserDao.java
@@ -1,8 +1,10 @@
 package edu.cnm.deepdive.tilematch.model.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
 import androidx.room.Update;
 import edu.cnm.deepdive.tilematch.model.entity.Game;
 import edu.cnm.deepdive.tilematch.model.entity.User;
@@ -39,5 +41,8 @@ public interface UserDao {
 
     @Delete
     Single<Integer> delete(Collection<? extends User> user);
+
+    @Query("SELECT * FROM game WHERE user_id = :userId")
+    LiveData<User> select(long userId);
 
 }
---
+++ app/src/main/java/edu/cnm/deepdive/tilematch/service/TileGaloreDatabase.java
@@ -0,0 +1,64 @@
+package edu.cnm.deepdive.tilematch.service;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
+import edu.cnm.deepdive.tilematch.model.dao.GameDao;
+import edu.cnm.deepdive.tilematch.model.dao.UserDao;
+import edu.cnm.deepdive.tilematch.model.entity.Game;
+import edu.cnm.deepdive.tilematch.model.entity.User;
+import edu.cnm.deepdive.tilematch.service.TileGaloreDatabase.Converters;
+import java.util.Date;
+
+@Database(
+    entities = {Game.class, User.class},
+    version = 1,
+    exportSchema = true
+)
+@TypeConverters({Converters.class})
+public abstract class TileGaloreDatabase extends RoomDatabase {
+
+  private static final String DB_NAME = "tileGalore-db";
+
+  private static Application context;
+
+  public static void setContext(Application context) {
+    TileGaloreDatabase.context = context;
+  }
+
+  public static TileGaloreDatabase getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public abstract GameDao getGameDao();
+
+  public abstract UserDao getUserDao();
+
+  private static class InstanceHolder {
+
+    private static final TileGaloreDatabase INSTANCE =
+        Room.databaseBuilder(context, TileGaloreDatabase.class, DB_NAME)
+        .build();
+
+  }
+
+  public static class Converters {
+
+    @TypeConverter
+    public static Long toLong(Date value) {
+      return(value != null) ? value.getTime() : null;
+    }
+
+    @TypeConverter
+    public static Date toDate(Long value) {
+      return(value != null) ? new Date(value) : null;
+    }
+
+  }
+
+
+
+}
